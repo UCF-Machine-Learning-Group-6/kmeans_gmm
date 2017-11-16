03bbac8e6d9ff9c896c1d0b33fe27fa8f6a3dc9f
Otaku-AI <rfatt13@gmail.com>
2017-11-15 20:28:01 -0500

    Graphs working

--- kmeans_gmm_sklearn.py
+++ kmeans_gmm_sklearn.py
@@ -16,49 +16,25 @@ from sklearn.model_selection import StratifiedKFold
 
 from sklearn.cluster import KMeans
 
-from sklearn import datasets
+from sklearn.metrics.pairwise import pairwise_distances_argmin
 
 import pandas as pd
 
-color_iter = itertools.cycle(['navy', 'c', 'cornflowerblue', 'gold',
 
-                              'darkorange'])
+colors = ['navy', 'turquoise', 'darkorange']
 
+X = pd.read_csv("ecoli_pruned_3k.csv", names=[
 
-def plot_results(X, Y_, means, covariances, index, title):
-
-    splot = plt.subplot(2, 1, 1 + index)
-    i = 0
-    for l, (mean, covar, color) in enumerate(zip(
-
-            means, covariances, color_iter)):
-
-        plt.scatter(i, Y_[i], .8, color=color)
-
-        i = i + 1
-
-    plt.xlim(-9., 5.)
-
-    plt.ylim(-3., 6.)
-
-    plt.xticks(())
-
-    plt.yticks(())
-
-    plt.title(title)
-
-
-X = pd.read_csv("ecoli_pruned_3k.csv", names=['1', '2', '3', '4'])
+    '1', '2', '3', '4'])
 
 X = X[['1', '2', '3']]
 
-est = KMeans(n_clusters=3)
+k_means = KMeans(n_clusters=3)
 
-est.fit(X)
+k_means.fit(X)
 
-labels = est.labels_
+labels = k_means.labels_
 
-X['labels'] = labels
 
 gmm = GaussianMixture(
 
@@ -66,13 +42,29 @@ gmm = GaussianMixture(
 
 gmm.fit(X)
 
+
+fig = plt.figure(figsize=(8, 3))
+fig.subplots_adjust(left=0.02, right=0.98, bottom=0.05, top=0.9)
+colors = ['#4EACC5', '#FF9C34', '#4E9A06']
+k_means_cluster_centers = np.sort(k_means.cluster_centers_, axis=0)
+k_means_labels = pairwise_distances_argmin(X, k_means_cluster_centers)
 X = X.values
+ax = fig.add_subplot(1, 3, 1)
+for k, col in zip(range(3), colors):
+    my_members = k_means_labels == k
+    cluster_center = k_means_cluster_centers[k]
+    ax.plot(X[my_members, 0], X[my_members, 1], 'w',
+            markerfacecolor=col, marker='.')
+    ax.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,
+            markeredgecolor='k', markersize=6)
 
-plot_results(X, gmm.predict(X), gmm.means_, gmm.covariances_, 0,
-
-             'Gaussian Mixture')
+ax.set_xticks(())
+ax.set_yticks(())
 
 plt.show()
 
+for n, color in enumerate(colors):
+    plt.scatter(X[:, 0], gmm.predict(X), s=0.8, color=color,
+                label=labels)
 
-# End of File
+plt.show()
